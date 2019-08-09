4f56ae7236c39f9c809e9e38e9ccd31ba5e1935a
Arinze George <arinzenne2017@gmail.com>
2019-08-09 17:39:23 +0100

    deleted keys

--- app/src/main/java/com/example/flutterwavebanktransfer/utils/Constants.java
+++ app/src/main/java/com/example/flutterwavebanktransfer/utils/Constants.java
@@ -4,8 +4,8 @@ public class Constants {
     static String UTF_8 = "utf_8";
     static String MD5 = "md5";
     static String ALGORITHM = "3DES-24";
-    static String TRANSFORMATION = "DESede";//DES/ECB/PKCS5Padding  /CBC/PKCS5Padding
+    static String TRANSFORMATION = "DESede";
     static String TARGET = "FLWSECK-";
-    static String PUBKEY = "FLWPUBK-0b5983334ffc7bd74f69443fa0501b67-X";
-    public static String SECKEY = "FLWSECK-ce73a9155b0a7c2ba6af3914780d2b54-X";
+    static String PUBKEY = "";
+    public static String SECKEY = "";
 }
